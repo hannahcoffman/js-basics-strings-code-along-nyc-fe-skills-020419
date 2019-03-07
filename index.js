132cc3e5b4f87ce9fb1b0779372b63eddeb0069c
Hannah Coffman <hcoffman7@gmail.com>
2019-03-07 01:33:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 const currentUser = 'Grace Hopper'
 const welcomeMessage = `Welcome to Flatbook, ' ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = ''
 
 
